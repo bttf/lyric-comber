78f995737e8a1b9be8948caaa292917769065fff
bttf <adnanchowdhury88@gmail.com>
2016-04-10 11:09:05 -0700

    refine ui

--- app/components/direct-lyric-search/component.js
+++ app/components/direct-lyric-search/component.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  areLyricsLoading: false,
+
   actions: {
     search(artist, title) {
+      this.set('areLyricsLoading', true);
+
       this.sendAction('searchLyrics', artist, title, (err, lyrics) => {
+        this.set('areLyricsLoading', false);
         if (err) { console.log(err); }
         else {
           this.sendAction('setLyrics', lyrics, artist, title);
